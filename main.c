3b1b86e9e08e91b9c2ff9bdc3a7a190273ebd712
Aaron Shenny <aaronshenny102.go@gmail.com>
2026-02-07 06:21:26 +0000

    hello-trial

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
-#include <direct.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
