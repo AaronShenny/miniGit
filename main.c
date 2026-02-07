273bbce283452dd37661a85bc7cda5014a883979
Aaron Shenny <aaronshenny102.go@gmail.com>
2026-02-07 06:32:58 +0000

    blahh

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -24,21 +25,59 @@ int  checkdir(){
 
 }
 
+int hash_file(char fileinput[]){
+	//hash the file and return the hash value
+	int hash = 5381;
+	FILE *fp = fopen(fileinput, "rb");
+	if (fp == NULL) {
+		printf("Error opening file: %s\n", fileinput);
+		return -1;
+	}
+	int c;
+	while ((c = fgetc(fp)) != EOF) {
+		hash =  hash * 33 + c ;
+	}
+	fclose(fp);
+	return hash;
+}
+
+void store_blob(char fileinput[]){
+	int hash = hash_file(fileinput);
+	char path[100];
+	sprintf(path, ".bit/objects/blobs/%d", hash);
+	FILE *fp = fopen(path, "wb");
+	if (fp == NULL) {
+		printf("Error opening file: %s\n", path);
+		return;
+	}
+	FILE *input_fp = fopen(fileinput, "rb");
+	if (input_fp == NULL) {
+		printf("Error opening file: %s\n", fileinput);
+		fclose(fp);
+		return;
+	}
+	int c;
+	while ((c = fgetc(input_fp)) != EOF) {
+		fputc(c, fp);
+	}
+	fclose(fp);
+	fclose(input_fp);
+}
 
 int main(int argc, char *argv[]) {
 	
 	if(argc<2){
 		printf("bit: <usage>");
-		return;
+		return 1;
 	}
 	if ( strcmp(argv[1], "init") == 0){
 		int check = checkdir();
 		if (check==1){
-			mkdir(".bit");
-			mkdir(".bit/objects");
-			mkdir(".bit/objects/blobs");
-			mkdir(".bit/objects/trees");
-			mkdir(".bit/commits");
+			mkdir(".bit", 0755);
+			mkdir(".bit/objects", 0755);
+			mkdir(".bit/objects/blobs", 0755);
+			mkdir(".bit/objects/trees", 0755);
+			mkdir(".bit/commits", 0755);
 			
 			FILE *fp = fopen(".bit/HEAD", "w");
 			fclose(fp);
@@ -58,4 +97,5 @@ int main(int argc, char *argv[]) {
 	else{
 		printf("Unknown Command!");
 	}
+	return 0;
 }
