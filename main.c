27f6462dec26b401194317b09d52df28c3ad1470
Aaron Shenny <aaronshenny102.go@gmail.com>
2026-02-07 11:41:25 +0000

    merge issue

--- main.c
+++ main.c
@@ -95,7 +95,7 @@ int main(int argc, char *argv[]) {
 		
 	}
 	else{
-		printf("bit: Unknown Command! Please use bit help\n");
+		printf("bit: Unknown Command! Please use `bit help`\n");
 	}
 	return 0;
 }
